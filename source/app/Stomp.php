9dd87f82962274ab22f4a1dc6ed9386f998ef632
hersonpc <hersonpc@gmail.com>
2019-06-14 11:11:12 -0300

    Fix - Get env variables

--- source/app/Stomp.php
+++ source/app/Stomp.php
@@ -148,71 +148,33 @@ class Stomp {
 
   private function stompClient() {
     if(is_null($this->clientInstance)) {
-      $this->clientInstance = new Client('tcp://192.168.6.1:61613');
-      $this->clientInstance->setLogin('mqadmin', 'isgtecnologia_');
-      $this->clientInstance->setVhostname('/');
+        $this->clientInstance = new Client('tcp://'.$this->getHost().':'.$this->getPort());
+        $this->clientInstance->setLogin($this->getUsername(), $this->getPassword());
+        $this->clientInstance->setVhostname('/');
     }
     return($this->clientInstance);
   }
   
   public function sendMessage($message, $queueName = null) {
+        if(!is_null($queueName)) {
+            $this->setQueue($queueName);
+        }
+        $queueName = $this->getQueue();
 
-    if(!is_null($queueName)) {
-      $this->setQueue($queueName);
+        if (!is_string($message)) {
+            $message = json_encode($message);
+        }
+        try {
+            $stomp = $this->stompClient();
+            $out = $stomp->send($queueName, $message, array('persistent' => 'true'));
+        } catch (StompException $e) {
+            echo $e->getMessage() . "\n";
+        }
+
+        $stomp->disconnect();
+        return($out);
     }
-    $queueName = $this->getQueue();
-
-    if (!is_string($message)) {
-      $message = json_encode($message);
-    }
-      try {
-          $stomp = $this->stompClient();
-          $out = $stomp->send($queueName, $message, array('persistent' => 'true'));
-      } catch (StompException $e) {
-          echo $e->getMessage() . "\n";
-      }
-
-      $stomp->disconnect();
-      return($out);
-  }
   
-  // public function consumer($queueName = null) {
-  //   if(is_null($queueName)) {
-  //       $queueName = $this->getQueue();
-  //   }
-  //   $iterations = 0;
-  //   try {
-  //     $stomp = new StatefulStomp($this->stompClient());
-  //     $stomp->subscribe($queueName, null, 'client');
-  //     do {
-  //       $iterations++;
-  //       $frame = $stomp->read();
-  //       if(isset($frame) and is_object($frame) and property_exists($frame, 'body')) {
-  //         // Processamento
-  //         try {
-  //           print_r(json_encode(json_decode($frame->body), JSON_PRETTY_PRINT));
-  //         } catch (StompException $e) {
-  //           echo "[ERROR] " . $e->getMessage() . "\n";
-  //         }
-
-  //         if(random_int(0, 5) % 2 == 0) {
-  //             $stomp->ack($frame);
-  //             echo "\n<< [200] Success", PHP_EOL;
-  //         }
-  //         else {
-  //             $stomp->nack($frame);
-  //             echo "\n<< [400] Processing error!!!", PHP_EOL;
-  //         }
-  //       }
-  //     } while (True);
-  
-  //     $stomp->unsubscribe();
-  //   } catch (StompException $e) {
-  //     echo "[ERROR] producer cannot connect to RabbitMQ\n";
-  //     echo $e->getMessage() . "\n";
-  //   }
-  // }
-
     public function consumeCallback($callback, $queueName = null) {
         if(is_null($queueName)) {
             $queueName = $this->getQueue();
